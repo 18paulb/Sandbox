be737b48a9aaa0ecc8dad7bab3c258ecf6f738b4
Brandon <bjpaul99@gmail.com>
2022-11-12 18:26:37 -0700

    Added all ordres, fixed mapview

--- dashrfrontend/src/app/suggestedroute/suggestedroute.component.ts
+++ dashrfrontend/src/app/suggestedroute/suggestedroute.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit, Inject, Input } from '@angular/core';
-import { ViewEncapsulation } from '@angular/core';
-import { NonNullableFormBuilder } from '@angular/forms';
+
 
 import { Maps, Zoom, ILoadEventArgs, Marker, NavigationLine } from '@syncfusion/ej2-angular-maps';
 import { Injectable } from '@angular/core';
 import { HttpBackend, HttpParams, HttpClient } from '@angular/common/http';
 import { last, lastValueFrom } from 'rxjs';
 
+Maps.Inject(Zoom, Marker, NavigationLine)
+@Injectable()
 @Component({
   selector: 'app-suggestedroute',
   templateUrl: './suggestedroute.component.html',
@@ -36,8 +37,8 @@ export class SuggestedrouteComponent implements OnInit {
   public chickfilaLat = 40.250757
   public chickfilaLon = -111.667413
 
-  public wendysLat = 40.250437
-  public wendysLon = -111.656065
+  public wendysLat = 40.240365
+  public wendysLon = -111.646407
 
   public orders:any = []
 
@@ -49,14 +50,11 @@ export class SuggestedrouteComponent implements OnInit {
 
   ngOnInit(): void {
 
-    //debugger
-
     this.getOrders()
 
-    //Initial Will go to restaurant
-    this.route.push([this.wendysLat, this.wendysLon])
+    //this.helperGetRoute(this.startingLat, this.startingLon, this.orders)
 
-    //this.getRoute(this.wendysLat, this.wendysLon);
+    //this.makeLines()
 
     this.zoomSettings = {
       enable:true,
@@ -71,8 +69,9 @@ export class SuggestedrouteComponent implements OnInit {
       latitude: 40.24478630014135,
       longitude: -111.64564335092805
     };
-    /*
+
     this.navigationLineSettings = [
+      /*
     {
         visible: true,
         color: 'blue',
@@ -80,14 +79,56 @@ export class SuggestedrouteComponent implements OnInit {
         angle: 0.1,
         latitude: [34.06062, 40.724546],
         longitude: [-118.330491, -73.850344]
-    }];
+    },
     */
+    {
+      visible: true,
+      color: 'blue',
+      width: 5,
+      angle: 0.1,
+      latitude: [40.235728, 40.250437],
+      longitude: [-111.653169, -111.656065]
+    },
+    {
+      visible: true,
+      color: 'blue',
+      width: 5,
+      angle: 0.1,
+      latitude: [40.250437, 40.254631],
+      longitude: [-111.656065, -111.658409]
+    },
+    {
+      visible: true,
+      color: 'blue',
+      width: 5,
+      angle: 0.1,
+      latitude: [40.254631, 40.244261],
+      longitude: [-111.658409, -111.643687]
+    },
+    {
+      visible: true,
+      color: 'blue',
+      width: 5,
+      angle: 0.1,
+      latitude: [40.244261, 40.244474],
+      longitude: [-111.645321, -111.645321]
+    },
+    {
+      visible: true,
+      color: 'blue',
+      width: 5,
+      angle: 0.1,
+      latitude: [40.244474, 40.271862],
+      longitude: [-111.645321, -111.697146]
+    },
+  ];
+
   }
 
   public zoomSettings:object | undefined
   public centerPosition: object | undefined
   public markerSettings: any
-  public navigationLineSettings: object | undefined
+  public navigationLineSettings: any
 
   public bingKey = "ApscR-2i8TspsYdQ4QtRVWVVaM08wKQF2yozbQ3e_3BUriw0V5a1OJydo_9SPuF9"
 
@@ -168,6 +209,79 @@ export class SuggestedrouteComponent implements OnInit {
     });
   }
 
+  public makeLines() {
+    for (let i = 0; i < this.route.length; ++i) {
+      if (i + 1 == this.route.length) {
+        break;
+      }
+
+      let startLat = this.route[i][0]
+      let startLon = this.route[i][1]
+      let endLat = this.route[i + 1][0]
+      let endLon = this.route[i + 1][1]
+
+      this.navigationLineSettings.push({
+        visible: true,
+        color: 'blue',
+        width: 2,
+        angle: 0.1,
+        latitude: [startLat, endLat],
+        longitude: [startLon, endLon]
+      })
+    }
+  }
+
+  public async helperGetRoute(startLat:any, startLon:any, ordersLeft:any[]) {
+
+    this.route.push([startLat, startLon])
+    this.route.push([this.wendysLat, this.wendysLon])
+
+    let tmpTime = await this.getTime(startLat, startLon, this.wendysLat, this.wendysLon)
+    let distance = await this.getDistance(startLat, startLon, this.wendysLat, this.wendysLon);
+
+    this.predictedTime += tmpTime
+    this.distanceTravelled += distance
+
+    //debugger
+
+    await this.getRoute(this.wendysLat, this.wendysLon, ordersLeft)
+
+    //Draw Lines
+
+    console.log("Final Route: ", this.route)
+
+    debugger
+
+    // this.navigationLineSettings = []
+/*
+    for (let i = 0; i < this.route.length; ++i) {
+      if (i + 1 == this.route.length) {
+        break;
+      }
+
+      let startLat = this.route[i][0]
+      let startLon = this.route[i][1]
+      let endLat = this.route[i + 1][0]
+      let endLon = this.route[i + 1][1]
+
+      //let newJson = {visible: true, color: 'blue', width: 5, angle: .1, latitude: [startLat, endLat], longitude: [startLon, endLon]}
+      //this.navigationLineSettings.push(newJson)
+
+      this.navigationLineSettings.push({
+        visible: true,
+        color: 'blue',
+        width: 2,
+        angle: 0.1,
+        latitude: [startLat, endLat],
+        longitude: [startLon, endLon]
+      })
+
+      console.log("Navigation: " + this.navigationLineSettings)
+    }
+    */
+
+  }
+
   public async getTime(lat1: any, lon1: any, lat2: any, lon2: any) {
     return fetch("http://dev.virtualearth.net/REST/V1/Routes?wp.0=" + lat1 + "," + lon1 + "&wp.1=" + lat2 + "," + lon2 + "&key=" + this.bingKey)
     .then((response) => response.json())
@@ -179,22 +293,49 @@ export class SuggestedrouteComponent implements OnInit {
       //Converts to Minutes
       time = time / 60
 
-      console.log("Time: " + time + " minutes")
-
       return time
     });
   }
 
-  public async getRoute(startLat:any, startLon:any) {
-    for (let order of this.orders) {
+  public async getRoute(startLat:any, startLon:any, ordersLeft:any[]) {
+
+    if (ordersLeft.length == 0) {
+      return
+    }
+
+    let smallestDistance = 100
+    let closestOrder = ordersLeft[0]
+
+    for (let order of ordersLeft) {
       let distance = await this.getDistance(startLat, startLon, order.latitude, order.longitude);
-      console.log(distance)
-      /*
+      //console.log(distance)
       if (distance < smallestDistance) {
+        closestOrder = order
         smallestDistance = distance
       }
-      */
     }
+
+    //debugger
+
+    let tmpTime = await this.getTime(startLat, startLon, closestOrder.latitude, closestOrder.longitude)
+
+    this.distanceTravelled = Math.round(((this.distanceTravelled + smallestDistance) * 10) / 10)
+    this.predictedEarnings = Math.round(((this.predictedEarnings + closestOrder.price) * 100) / 100)
+    this.predictedTime = Math.round(this.predictedTime + tmpTime)
+
+    for (let i = 0; i < ordersLeft.length; ++i) {
+      if (ordersLeft[i].orderID == closestOrder.orderID) {
+        ordersLeft.splice(i,1)
+        break
+
+      }
+    }
+
+    //We have smallest distance
+    this.route.push([closestOrder.latitude, closestOrder.longitude])
+    console.log("Route: ", this.route)
+
+    await this.getRoute(closestOrder.latitude, closestOrder.longitude, ordersLeft)
   }
 
 }
